08c33fb6bd38ad1934308f8e3796f0804fab2940
Canek García <kanect@rocketmail.com>
2018-10-25 19:07:46 -0500

    Initial commit

---
+++ 00Introduction/introduction.py
@@ -0,0 +1,55 @@
+### 0. hola mundo ###
+print("Hola mundo")
+
+
+### 1. Números y operadores aritméticos ###
+2 + 2
+20 - 10
+2 * 9
+20 / 2 
+
+8 + 2 * 10
+(8 + 2) * 10
+
+19/2 #9.5
+19//2 #9
+
+2 ** 5 # 2^5
+
+
+### 2. Variables ###
+x = 2
+y = 6 ** 3
+x + 2
+x = x + 2
+x = x * 3
+z = 19/2
+a = "Hola mundo"
+
+desayuno = "Huevos con tocino"
+x = x + y
+
+x = "sandwich"
+
+
+### 3. Cadenas ###
+# nombre = "Canek"
+nombre = 'Canek'
+
+# frase = "Ella me dijo: 'Hola'"
+# frase = 'Ella me dijo: "Hola"'
+# frase = "Ella me dijo: \"Hola\""
+# frase = 'Ella me dijo: \'Hola\''
+frase = "Ella me dijo: \n\"Hola\""
+
+saludo = "Hola"
+print(saludo + " " + nombre)
+
+print(nombre * 3)
+
+
+### 4. Condiciones ###
+edad = 21
+
+if edad == 21:
+    print("Puedes entrar")
