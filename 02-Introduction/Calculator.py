e5722566f2a05c83857bc7103023008a9fe6857a
Canek García <kaan.ek@ciencias.unam.mx>
2023-02-07 04:10:15 -0600

    35. Calculadora II

--- 02-Introduction/Calculator.py
+++ 02-Introduction/Calculator.py
@@ -1,30 +1,39 @@
 
 def operaciones(opcion, numero_a, numero_b):
-    if opcion == '1':
+    if opcion == 1:
         return numero_a + numero_b
-    elif opcion == '2':
+    elif opcion == 2:
         return numero_a - numero_b
-    elif opcion == '3':
+    elif opcion == 3:
         return numero_a * numero_b
-    elif opcion == '4':
+    elif opcion == 4:
         return numero_a / numero_b
     else:
         print('Opción inválida')
 
 
 print('Bienvenido a la calculadora')
-print('Estas son las operaciones que puedes realizar:')
-print('1 - Suma')
-print('2 - Resta')
-print('3 - Multiplicación')
-print('3 - División')
-
-opcion = input('Introduce el número de operación que quieres realizar: ')
-numero_a = int(input('Introduce el primer número: '))
-numero_b = int(input('Introduce el segu1ndo número: '))
-
-numero_c = operaciones(opcion, numero_a, numero_b)
-print('El resultado es: ' + str(numero_c))
+while True:
+    print('Estas son las operaciones que puedes realizar:')
+    print('1 - Suma')
+    print('2 - Resta')
+    print('3 - Multiplicación')
+    print('3 - División')
 
 
-#continuar = input("¿Deseas continuar? si/no")
+    try:
+        opcion = int(input('Introduce el número de operación que quieres realizar: '))
+        numero_a = int(input('Introduce el primer número: '))
+        numero_b = int(input('Introduce el segu1ndo número: '))
+    except ValueError:
+        print('Opción o número inválido')
+    else:
+        numero_c = operaciones(opcion, numero_a, numero_b)
+        print('El resultado es: ' + str(numero_c))
+        
+        continuar = input("¿Deseas continuar? [si/no] ")
+        # if continuar == "no":
+        if continuar != "si":
+            break
+
+print('¡Gracias!')
